3a58101c5e7afdb2fb9ee814b2593c5db48306d1
Bibek Mainali <mainalibibek01@gmail.com>
2026-03-18 23:04:25 +0545

    Dev: Increated timeout to 100000.

--- bugtrackingsystem.client/src/services/api.js
+++ bugtrackingsystem.client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://localhost:7088/api',
-    timeout: 5000,
+    timeout: 100000,
     headers: { 'Content-Type': 'application/json' },
 });
 export default api;
