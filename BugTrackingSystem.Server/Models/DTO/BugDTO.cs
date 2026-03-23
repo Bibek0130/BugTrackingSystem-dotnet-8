69751d10078a0fddeb9c78c3ad34d6b7f4c151fe
Bibek Mainali <mainalibibek01@gmail.com>
2026-03-24 00:53:24 +0545

    Dev: Defined BBugDTO

---
+++ BugTrackingSystem.Server/Models/DTO/BugDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BugTrackingSystem.Server.Models.DTO
+{
+    public class BugDTO
+    {
+        
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        //public string? Description { get; set; }
+        //public string? Status { get; set; }
+        //public string? Severity { get; set; }
+        //public DateTime CreatedDate { get; set; }
+    }
+}
