6b42d74d7d8f7fcefa01dd71d2b57b51f6865e32
Bibek Mainali <mainalibibek01@gmail.com>
2026-03-25 23:39:09 +0545

    Dev: Created BugDTO.

--- BugTrackingSystem.Server/Models/DTO/BugDTO.cs
+++ BugTrackingSystem.Server/Models/DTO/BugDTO.cs
@@ -1,13 +1,20 @@
-﻿namespace BugTrackingSystem.Server.Models.DTO
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTrackingSystem.Server.Models.DTO
 {
     public class BugDTO
     {
-        
-        public int Id { get; set; }
+        [Column("Id")]
+        public int? Id { get; set; }
+        [Column("Title")]
         public string? Title { get; set; }
-        //public string? Description { get; set; }
-        //public string? Status { get; set; }
-        //public string? Severity { get; set; }
-        //public DateTime CreatedDate { get; set; }
+        [Column("Description")]
+        public string? Description { get; set; } = null;
+        [Column("Status")]
+        public string? Status { get; set; }
+        [Column("Severity")]
+        public string? Severity { get; set; }
+        [Column("CreatedDate")]
+        public DateTime? CreatedDate { get; set; }
     }
 }
