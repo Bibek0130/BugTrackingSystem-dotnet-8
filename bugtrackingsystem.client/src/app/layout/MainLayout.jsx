d93be1cf280912057c83dbe866ff709a2fb107ca
Bibek Mainali <mainalibibek01@gmail.com>
2026-03-26 23:30:31 +0545

    Fix: Fixed width issue in body.

--- bugtrackingsystem.client/src/app/layout/MainLayout.jsx
+++ bugtrackingsystem.client/src/app/layout/MainLayout.jsx
@@ -17,40 +17,47 @@ const CustomNav = ({ li }) => {
 
     };
     return (
-        <div className="app-container">
-            <nav
-                className="navbar-menu"
-                onClick={openClose}
-                style={{ width: window === false ? 250 : 60 }}
-            >
-                <div className="burger" zxc>
-                    <img src="menu.png" alt="burger" />
+        <div className="container-fluid">
+            <div className="container-fluid row ">
+                <div className="col-lg col-lg-3 app-container ">
+                    <nav
+                        className="navbar-menu"
+                        onClick={openClose}
+                        style={{ width: window === false ? 250 : 60 }}
+                    >
+                        <div className="burger" zxc>
+                            <img src="menu.png" alt="burger" />
+                        </div>
+                        <ul className="navbar__list">
+                            {li.map((item, i) => {
+                                const path = `/${item[0].toLowerCase().replace(/\s/g, "-")}`;
+                                return (
+                                    <li key={i} className="navbar__li-box">
+                                        <Link to={path} style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
+                                            <img
+                                                src={item[1]}
+                                                alt={item[0]}
+                                                style={{ paddingLeft: window ? 17 : 27, width: '20px' }}
+                                            />
+                                            {!window && (
+                                                <span className="navbar__li" style={{ marginLeft: '10px' }}>
+                                                    {item[0]}
+                                                </span>
+                                            )}
+                                        </Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </nav>
                 </div>
-                <ul className="navbar__list">
-                    {li.map((item, i) => {
-                        const path = `/${item[0].toLowerCase().replace(/\s/g, "-")}`;
-                        return (
-                            <li key={i} className="navbar__li-box">
-                                <Link to={path} style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
-                                    <img
-                                        src={item[1]}
-                                        alt={item[0]}
-                                        style={{ paddingLeft: window ? 17 : 27, width: '20px' }}
-                                    />
-                                    {!window && (
-                                        <span className="navbar__li" style={{ marginLeft: '10px' }}>
-                                            {item[0]}
-                                        </span>
-                                    )}
-                                </Link>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </nav>
-            <main style={{ marginLeft: window === false ? 20 : 50, transition: 'margin-left 0.3s' }}>
-                <Outlet />
-            </main>
+                <div className="col-lg col-md-auto">
+                    <main style={{ marginLeft: window === false ? 20 : 50, transition: 'margin-left 0.3s' }}>
+                        <Outlet />
+                    </main>
+                </div>
+               
+            </div>     
         </div>
     );
 };
