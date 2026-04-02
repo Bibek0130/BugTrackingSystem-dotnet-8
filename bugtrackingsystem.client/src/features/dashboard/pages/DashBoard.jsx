061590a01dddf6d9cb84bb8d096f0af11eb0ddf8
Bibek Mainali <mainalibibek01@gmail.com>
2026-04-02 14:39:26 +0545

    Dev: Created divs for dashboard test.

--- bugtrackingsystem.client/src/features/dashboard/pages/DashBoard.jsx
+++ bugtrackingsystem.client/src/features/dashboard/pages/DashBoard.jsx
@@ -1,9 +1,39 @@
 function Dashboard() {
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+        <>
+            <div className="header">
+                <h1>Dashboard</h1>
+            </div>
+
+            <div className="container-fluid">
+                <div className="row" style={blueColor}>
+                    <div className="col-lg-4 col-12" style={ redColor}>Red
+                    </div>
+                    <div className="col-lg-4 col-12"> Blue
+                    </div>
+                    <div className="col-lg-4 col-12" style={yellowColor}> White
+                    </div>
+                </div>
+            </div>
+        </>
+      
     );
 }
 
 export default Dashboard;
+
+const redColor = {
+    backgroundColor: "red",
+    width: "200px",
+    innerHeight: "200px",
+
+}
+const blueColor = {
+    backgroundColor: "blue"
+    
+}
+const yellowColor = {
+    backgroundColor: "yellow"
+
+}
+
