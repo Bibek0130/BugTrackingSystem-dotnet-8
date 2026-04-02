631135d277428e0bfdec63b54621ae384885ebef
Bibek Mainali <mainalibibek01@gmail.com>
2026-04-03 01:22:43 +0545

    Dev: Added div for bugscount and bugs closed.

--- bugtrackingsystem.client/src/features/dashboard/pages/DashBoard.jsx
+++ bugtrackingsystem.client/src/features/dashboard/pages/DashBoard.jsx
@@ -3,16 +3,55 @@ import api from '../../../services/api';
 import { useEffect } from 'react';
 function Dashboard() {
     const [bugCount, setBugCount] = useState(); // Example bug count, replace with actual data
+    const [bugClosed, setBugClosed] = useState();
+    const [fetching, setFetching] = useState(true); // State to track if data is being fetched
 
-    fetchBugCount(); // Call the function to fetch bug count when the component mounts
+    useEffect(() => {
+        if (fetching) {
+            try {
+                fetchBugCount();
+                getClosedBugs();
+            }
+            catch (err) {
+                console.log("Error fetching data:", err);
+            }
+            finally {
+                setFetching(false); // Set fetching to false after data is fetched
+            }
+        }
+    },[]);  
+  
+
     //function for fetching bug count from the API
     async function fetchBugCount() {
         try {
             const response = await api.get("Bugs/bugs-count");
 
             if (response.status >= 200 && response.status < 300) {
+                console.log("Bug Count", response);
                 const length = response.data;
-                setBugCount(length); // Assuming response.data is an array of bugs
+
+                //set the value to the state
+                setBugCount(length);
+            }
+        } catch (err) {
+            console.log("Error fetching bug count:", err);
+        }
+        finally {
+            setFetching(false);
+        }
+    }
+    //function for fetching bug count from the API
+    async function getClosedBugs() {
+        try {
+            const response = await api.get("Bugs/closed-bugs-count");
+
+            if (response.status >= 200 && response.status < 300) {
+                console.log("Bug Closed", response);
+                const dd = response.data;
+
+                //set the value to the state
+                setBugClosed(dd);
             }
         } catch (err) {
             console.log("Error fetching bug count:", err);
@@ -25,13 +64,21 @@ function Dashboard() {
                 <h1>Dashboard</h1>
             </div>
 
-            <div className="container-fluid">
-                <div className="row" style={BugCount }>
+            <div className="container-fluid p-2">
+                <div className="row p-6" style={BugCount }>
                     <div className="col-lg-4 ">
                         <p>Total Bugs Created</p>
                     </div>
                     <div className="col-lg-4 ">{bugCount }
                     </div>                  
+                </div>
+
+                <div className="row p-6" style={BugClosedCss}>
+                    <div className="col-lg-4 ">
+                        <p>Bugs Closed: </p>
+                    </div>
+                    <div className="col-lg-4 ">{bugClosed}
+                    </div>
                 </div>
             </div>
         </>
@@ -46,7 +93,13 @@ const BugCount = {
     border: "2px solid forestgreen",
    // width: "200px",
     //height:"150px",
-    padding: "5%"
-
+    padding: "5%",
+    margin: "2%" //for adding gap and a nice look to it
+}
+const BugClosedCss ={
+    borderRadius: "10px",
+    border: "2px solid forestgreen",
+    padding: "5%",
+    margin: "2%"
 }
 
